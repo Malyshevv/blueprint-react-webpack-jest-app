9765628a71fafe4dd8ceb0e7399e9574ab9de3be
Ruvim <malyshev.dev@ya.ru>
2021-11-17 12:59:39 +0300

    blueprint-react-webpack-jest-app

--- src/server/server.js
+++ src/server/server.js
@@ -16,31 +16,6 @@ if(!isDev) {
     app.use(helmet())
 }
 
-app.get('/auth', (req,res) => {
-    let urlApi = "https://www.reddit.com/api/v1/access_token"
-    let grant_type = "authorization_code"
-    let urlRedirect = "https://redditappsb.herokuapp.com/auth"
-    let code = req.query.code
-
-    let username = process.env.CLIENT_ID
-    let password = process.env.CLIENT_SECRET
-
-    axios.post(
-        urlApi,
-        `grant_type=${grant_type}&code=${code}&redirect_uri=${urlRedirect}`,
-        {
-            auth: {username: username, password: password},
-            headers: { 'Content-type': 'application/x-www-form-urlencoded' }
-        }
-    )
-        .then(({ data })=> {
-            res.send(
-                indexTemplate(ReactDOM.renderToString(App()), data['access_token'])
-            );
-        })
-        .catch(console.log)
-})
-
 app.get('*', (req,res) => {
     res.send(
         indexTemplate(ReactDOM.renderToString(App()))

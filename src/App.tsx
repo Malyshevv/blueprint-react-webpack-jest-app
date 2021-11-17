865d6002d79e38ff29bd75a4b488f68074995b2e
Ruvim <malyshev.dev@ya.ru>
2021-11-17 12:30:13 +0300

    blueprint-react-webpack-jest-app

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 /**Project CSS**/
 import './main.global.css'
+/**utils**/
 
+/**My Component**/
+import {Layout} from "./shared/Layout";
+import {Content} from "./shared/Content";
+import {Header} from "./shared/Header";
+import {Footer} from "./shared/Footer";
 
 function AppComponent() {
-    return (<div>
-        hello
-    </div>)
+    return (
+        <Layout>
+            <Header/>
+            <Content>
+                Content
+            </Content>
+            <Footer/>
+        </Layout>
+    );
 }
 
 export const App = hot(() => <AppComponent/>);

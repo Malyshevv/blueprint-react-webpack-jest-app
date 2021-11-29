31828edd291b651a17f946b103d1526054ef1889
Malyshev R <malyshev.dev@ya.ru>
2021-11-29 18:46:07 +0300

    blueprint-react-webpack-jest-app

--- src/server/indexTemplate.js
+++ src/server/indexTemplate.js
@@ -1,11 +1,10 @@
-export const indexTemplate = (content,token) => `
+export const indexTemplate = (content) => `
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>My Site</title>
         <script src="/static/client.js" type="application/javascript"></script>
-        <script>window.__token__ = '${token}' </script>
     </head>
     <body>
     <div id="App">${content}</div>
